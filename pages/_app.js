945470902b8e4d044aca9be451134a18b5b62b7a
Your Name <you@example.com>
2020-12-25 18:00:11 -0500

    info card design completed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import Head from "next/head"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} >
+    <Head>
+      <script src="https://kit.fontawesome.com/87121b56c7.js" crossorigin="anonymous"></script>
+    </Head>
+  </Component>
 }
 
 export default MyApp
--- src/Human/components/InfoCard.js
+++ src/Human/components/InfoCard.js
@@ -3,6 +3,10 @@ import {
   infoContainer,
   sectionContainer,
   mainContainer,
+  rightInfo,
+  personalList,
+  schoolInfo,
+  bioInfo
 } from "./InfoCard.module.css";
 
 export const InfoCard = () => {
@@ -18,37 +22,37 @@ export const InfoCard = () => {
       </section>
 
       <section className={sectionContainer}>
-        <h3>Personal Info</h3>
+        <h3><span>Personal Info</span></h3>
         <ul>
-          <li>Random data</li>
-          <li>Random data</li>
-          <li>Random data</li>
-          <li>Random data</li>
-          <li>Random data</li>
+          <li className={personalList}><i className="far fa-clock"></i> <p>Age</p> <p className={rightInfo}>20</p> </li>
+          <li className={personalList}><i className="far fa-clock"></i> <p>Height</p> <p className={rightInfo}>5ft 4in</p> </li>
+          <li className={personalList}><i className="far fa-clock"></i> <p>Weight</p> <p className={rightInfo}>83kg</p> </li>
+          <li className={personalList}><i className="far fa-clock"></i> <p>Age</p> <p className={rightInfo}>20</p> </li>
+          <li className={personalList}><i className="far fa-clock"></i> <p>Age</p> <p className={rightInfo}>20</p> </li>
         </ul>
       </section>
       <section className={sectionContainer}>
-        <h3>School</h3>
-        <div>
+        <h3><span>School</span></h3>
+        <div className={schoolInfo}> 
           <h4>School</h4>
           <p>St. Jhon River</p>
         </div>
-        <div>
+        <div  className={schoolInfo}>
           <h4>School Year</h4>
           <p>Junior</p>
         </div>
-        <div>
+        <div  className={schoolInfo}>
           <h4>Summer Team</h4>
           <p>Scorpions</p>
         </div>
-        <div>
+        <div  className={schoolInfo}>
           <h4>College commitment</h4>
           <p>University of florida</p>
         </div>
       </section>
       <section className={sectionContainer}>
-        <h3>Bio</h3>
-        <p>
+        <h3><span>Bio</span></h3>
+        <p className={bioInfo}>
           US, baseball. Set numerous baseball records - was famousfor his big
           hitting. he won many titles with the New York Yankees
         </p>
